8c435dac195589717d230097236876f394c6ba21
Manjil Tamang <lamamanjil@gmail.com>
2024-10-22 23:50:31 -0400

    minor updates

--- interfaces/ITransaction.ts
+++ interfaces/ITransaction.ts
@@ -1,7 +1,7 @@
 import { CategoryType } from "@/types";
 
 export default interface ITransaction {
-  transactionDate: Date;
+  transactionDate: Date | string;
   title: string;
   category: CategoryType;
   amount: number;
