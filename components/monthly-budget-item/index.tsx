5851081a28c9a30bcbe8f6858bbcaf07561db57b
Manjil Tamang <lamamanjil@gmail.com>
2024-10-22 22:45:49 -0400

    minor UI updates

--- components/monthly-budget-item/index.tsx
+++ components/monthly-budget-item/index.tsx
@@ -10,22 +10,20 @@ export default function MonthlyBudgetItem({
   currency: string;
 }) {
   return (
-    <div className="border-b-[1px] border-neutral-700">
-      <div className="flex flex-wrap hover:bg-neutral-700 rounded-md -mx-2 space-x-2 ">
-        <div className="ml-2">
+    <div className="border-b-[1px] border-neutral-700 ">
+      <div className="flex flex-wrap hover:bg-neutral-700 rounded-md -mx-2 space-x-2 py-1">
+        <div className="ml-2 grow md:w-auto w-full">
           {categoryToEmoji(transaction.category)}
-          <input
-            type="text"
-            value={transaction.title}
-            className="grow md:w-auto w-full"
-          />
+          <input type="text" value={transaction.title} />
         </div>
-        <label className="relative">
-          <span className="text-neutral-500 absolute top-[8px] -left-[2px]">
-            {formatCurrency(currency)}
-          </span>
-          <input type="text" value={transaction.amount} className="w-20" />
-        </label>
+        <div>
+          <label className="relative ml-2">
+            <span className="text-neutral-500 absolute -top-[2px] -left-[6px]">
+              {formatCurrency(currency)}
+            </span>
+            <input type="text" value={transaction.amount} className="w-20" />
+          </label>
+        </div>
         <select
           name="category"
           defaultValue={transaction.category}
