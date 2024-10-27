ae81c6ec13824f498a0dc4965c893d3d7a7cd97b
Manjil Tamang <lamamanjil@gmail.com>
2024-10-27 02:08:35 -0400

    fix: next auth is redirecting too many times 3

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.googleapis.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+    domains: ["avatars.githubusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
